6e910d3f83f59ac7599d3585140d1839d5bc0b19
Ayipz <muhammadarief1234516@gmial.com>
2025-01-13 04:23:44 +0700

    fix: error assets

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { assets } from "@/assetsssets/assets";
+import { assets } from "@/assets/assets";
 
 const Footer = () => {
   return (
